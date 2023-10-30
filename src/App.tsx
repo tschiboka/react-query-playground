4d6d008f1c7d01e0d2685397c66438dcf0bbe356
Tivadar Debnar <tibi.aki.tivadar@gmail.com>
2023-10-30 23:38:54 +0000

    Build a Simple React Hook Form

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,71 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import { useForm } from "react-hook-form";
+import "./App.css";
+
+let renderCount = 0;
 
 function App() {
-  const [count, setCount] = useState(0)
+    renderCount++;
 
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    const {
+        register,
+        handleSubmit,
+        formState: { errors }, // Subscribe to Error Handling
+    } = useForm({
+        defaultValues: {
+            // Optionally Provide a Default Value Object
+            firstName: "John",
+            lastName: "Doe",
+        },
+    });
+
+    console.log(errors);
+
+    return (
+        <div>
+            <p>{renderCount}</p>
+
+            <form
+                onSubmit={handleSubmit((data) => {
+                    console.log(data);
+                })}
+            >
+                <input
+                    type="text"
+                    {...register("firstName", {
+                        required: "First Name is Required",
+                        minLength: {
+                            value: 3,
+                            message:
+                                "First Name Must be Minimum 3 Characters Long",
+                        },
+                    })}
+                    placeholder="First Name"
+                />
+                {errors?.firstName?.message && (
+                    <p>{errors?.firstName?.message}</p>
+                )}
+                <input
+                    type="text"
+                    {...register("lastName", {
+                        required: "Last Name is Required",
+                        minLength: {
+                            value: 3,
+                            message:
+                                "First Name Must be Minimum 3 Characters Long",
+                        },
+                    })}
+                    placeholder="Last Name"
+                />
+                {errors?.lastName?.message && (
+                    <p>{errors?.lastName?.message}</p>
+                )}
+                <input type="submit" />
+            </form>
+        </div>
+    );
 }
 
-export default App
+export default App;
